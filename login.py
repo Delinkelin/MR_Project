a76df6d2d1e3196a91e704049651c3b99936c9c5
Delinkelin <74640098+Delinkelin@users.noreply.github.com>
2022-05-27 10:59:16 +0530

    final

--- login.py
+++ login.py
@@ -37,24 +37,6 @@ for username, user_id in zip(user_df["usernames"], user_df["User-id"]):
     user_id_map[username] = str(user_id)
 tmdb_mov_map = pickle.load(open('tmdb_mov_map.pkl', 'rb'))
 title_mov_map = pickle.load(open('title_mov_map.pkl', 'rb'))
-# users_dict = pickle.load(open('watched1.pkl', 'rb'))
-# users_map={671:'dpatel', 672:'dvyas'}
-
-# names = ['Devasy Patel', 'Dhruvil Vyas']
-# usernames = ['dpatel', 'dvyas']
-# passwords = ['123', '123']
-# hashed_passwords = stauth.Hasher(passwords).generate()
-# authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
-#     'some_cookie_name', 'some_signature_key', cookie_expiry_days=30)
-# name, authentication_status, username = authenticator.login('Login', 'main')
-# if authentication_status:
-#     authenticator.logout('Logout', 'main')
-#     st.write('Welcome *%s*' % (name))
-#     st.title('Some content')
-# elif authentication_status == False:
-#     st.error('Username/password is incorrect')
-# elif authentication_status == None:
-#     st.warning('Please enter your username and password')
 
 
 def main():
@@ -63,12 +45,161 @@ def main():
         st.session_state['key'] = ""
     if(st.session_state['key'] == ""):
         with placeholder1.container():
-            st.title("Login/Signup")
+            # st.title("Login/Signup")
             menu = ["Home", "Login", "Signup"]
             choice = st.sidebar.selectbox("Menu", menu)
 
             if choice == "Home":
                 st.subheader("Home")
+                # placeholder1.empty()
+                watched_movies = []
+                # with pickle
+                # with open("users.csv", "r+") as f:
+                #     file = f.readlines()
+                #     recmovi = set(file)
+                #     pass
+                try:
+
+                    # recmovi = pickle.load(open('pick.pkl', 'rb'))
+                    watched_movies = pickle.load(open('watched.pkl', 'rb'))
+                except Exception:
+                    print("hello")
+
+
+                def fetch_posterpath(recomended_movies_set):
+                    for i in recomended_movies_set:
+                        dict = pickle.load(open('posterpaths.pkl', 'rb'))
+                        if i not in dict:
+                            st.write("Movie name is here", i)
+
+                            index = movies[movies['title'] == i].index[0]
+                            movie_id = movies.loc[index, 'movie_id']
+                            # st.write(movie_id)
+
+                            response = requests.get(
+                                f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=5f22100b0a8f34d1ea15eb5605bc2d87&language=en-US")
+                            response = response.json()
+
+                            data = response["poster_path"]
+                            poster = f"https://image.tmdb.org/t/p/w500/{data}"
+                            name = movies.loc[index, 'title']
+
+                            dict[name] = poster
+                            pickle.dump(dict, open('posterpaths.pkl', 'wb'))
+                    return dict
+
+                def fetch_posterpath_from_pkl(recomended_movies_set):
+                    dict = {}
+                    poster_dict_from_pkl = pickle.load(open('posterpaths.pkl', 'rb'))
+                    for i in recomended_movies_set:
+                        poster = poster_dict_from_pkl[i]
+                        dict[i] = poster
+                    return dict
+                    
+
+                def recommend(movie):
+                    movie_index = movies[movies['title'] == movie].index[0]
+
+                    distances = similarity[movie_index]
+                    movie_list = sorted(list(enumerate(distances)),
+                                        reverse=True, key=lambda x: x[1])
+                    movie_list = movie_list[1:7 + 1]
+                    # st.write(movie_list)
+                    # st.write(movie_list)
+                    list1 = []
+                    for i in movie_list:
+                        movie_id = movies.loc[i[0], 'movie_id']
+
+                        # response = requests.get(
+                        #     f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=5f22100b0a8f34d1ea15eb5605bc2d87&language=en-US")
+                        # response = response.json()
+
+                        # data = response["poster_path"]
+                        # poster = f"https://image.tmdb.org/t/p/w500/{data}"
+                        name = movies.loc[i[0], 'title']
+                        list1.append(name)
+                        # print(name)
+                        # print(type(name))
+                        # l.append(poster)
+                        # poster = 
+                        # l_dict[name] = [poster[name],i[1]]
+                        # l.append([poster[name], name])
+                    l_dict = fetch_posterpath_from_pkl(list1)
+                    l = dict2dlist(l_dict)
+                    return l, l_dict
+
+
+                # movi.append()
+                movies = pickle.load(open('movies.pkl', 'rb'))
+                movies = pd.DataFrame(movies)
+                similarity = pickle.load(open('similarity.pkl', 'rb'))
+                similarity = pd.DataFrame(similarity)
+
+                # # TO BE removed 
+                # try:
+                #     poster_path_dict = fetch_posterpath(movies['title'])
+                # except Exception as e:
+                #     st.write("No movies found")
+                # finally:
+                #     pickle.dump(poster_path_dict, open('posterpaths.pkl', 'wb'))
+
+                st.title('Quick Recommendations')
+                option = st.selectbox('Enter Movie Name', (movies['title']))
+                result = st.button("Recommend")
+
+
+                def dict2dlist(dict):
+                    l = []
+                    for x, y in dict.items():
+                        temp = [y, x]
+                        l.append(temp)
+                    return l
+
+
+                def display_from_dict(dict, message):
+                    l = dict2dlist(dict)
+                    img = []
+                    caption = []
+                    for i in range(len(l)):
+
+                        img.append(l[i][0])
+                        caption.append(l[i][1])
+                    # st.columns here since it is out of beta at the time I'm writing this
+                    # cols = cycle(st.columns(4))
+                    # for idx, filteredImage in enumerate(img):
+                    #     next(cols).image(filteredImage, width=150,
+                    #                      caption=caption[idx], use_column_width=False)
+                    st.write(message)
+                    st.image(img, width=155, caption=caption, use_column_width= False)
+
+
+                if result:
+                    output, output_dict = recommend(option)
+                    watched_movies.append(option)
+                    img, caption = [], []
+                    st.write("Based on your provided movie, we recommend the following movies")
+                    for i in range(7):
+                        # recmovi.add(output[i][1])
+                        img.append(output[i][0])
+                        caption.append(output[i][1])
+                    st.image(img, width=155, caption=caption, use_column_width= False)
+
+
+
+                    
+                        
+                    
+                # api key - 5f22100b0a8f34d1ea15eb5605bc2d87
+                # api - application programme interface (data pipeline)(software intersection)
+                # eg - irctc,mmt
+                # database calling - return data(json format string)
+                # dict ={
+                #     "devasy": {mo}
+                # }
+                    
+                # st.write(fetch_posterpath(watched_movies))
+               
+
             elif choice == "Login":
                 st.subheader("Login Section")
                 username = st.text_input("User name")
@@ -271,8 +402,7 @@ def main():
                     similar_movies = similar_movies.append(
                         get_similar(moviename, rating), ignore_index=True)
                 similar_movies.head(10)
-                x = []
-                print(similar_movies.sum().head(20))
+                x = [] # to store final recommendations
                 for y in similar_movies.sum().sort_values(ascending=False).head(20).index:
                     if y not in user_dict:
                         x.append(y)
